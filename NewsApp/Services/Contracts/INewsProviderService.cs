df96bec17e35c59d7b91b36adff8ea9ccc8863d5
Thaveesha Kannangara <thaveesha.k@evicio.com>
2023-01-15 17:21:07 +0530

    Code fix

---
+++ NewsApp/Services/Contracts/INewsProviderService.cs
@@ -0,0 +1,12 @@
+﻿using NewsAPI.Models;
+using System.Threading.Tasks;
+
+namespace NewsApp.Services.Contracts
+{
+	public interface INewsProviderService
+	{
+		Task<ArticlesResult> GetTopNewsUpdates(TopHeadlinesRequest request);
+
+		Task<ArticlesResult> GetNewsAsync(EverythingRequest request);
+	}
+}
